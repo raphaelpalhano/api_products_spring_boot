791065bd1c00b9f49595418edd8ec13a3f334357
Raphael Angel <raphael1angel@hotmail.com>
2023-09-02 18:37:23 -0300

    update controller

--- src/main/java/com/myecommerce/store/controllers/ProductController.java
+++ src/main/java/com/myecommerce/store/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.myecommerce.store.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -85,7 +86,7 @@ public class ProductController {
 
 
   
-  @PatchMapping("/products/{id}")
+  @DeleteMapping("/products/{id}")
   public ResponseEntity<Object> deleteProduct(@PathVariable UUID id) {
 
     Optional<ProductModel> product = productRepository.findById(id);
